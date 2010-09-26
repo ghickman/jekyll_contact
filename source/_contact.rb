5f29c2bb63285a8b75fe590d89911e820178e8f8
George Hickman <george@ghickman.co.uk>
2010-09-26 12:18:18 +0100

    integrated sinatra app with jekyll
    - styled contact form
    - added server side checks in

--- source/_contact.rb
+++ source/_contact.rb
@@ -14,13 +14,36 @@ class Page
   end
 end
 
-get '/contact' do
+def contact
   page = Page.new
   content = haml :contact
   haml :contact, :layout=>:'_layouts/default', :locals=>{:page=>page, :content=>content}
 end
 
-post '/contact' do
-  Pony.mail(:to=>'george@ghickman.co.uk', :from=>"#{params[:mail]}", :subject=>"#{params[:subject]}", :body=>"#{params[:message]}")
-  redirect '/index.html'
+get '/contact' do
+  # create the variables that the layout will expect
+  @errors={}
+  # page = Page.new
+  # content = haml :contact
+  
+  # render contact.haml with Jekyll's layout
+  # haml :contact, :layout=>:'_layouts/default', :locals=>{:page=>page, :content=>content}
+  contact
 end
+
+post '/contact' do
+  @errors={}
+  @errors[:name] = 'No Anon allowed here.' if params[:name].nil? || params[:name].empty?
+  @errors[:email] = 'Sinatra needs an email to send your message from!' if params[:email].nil? || params[:email].empty?
+  @errors[:message] = 'No message?! Sounds like heavy breathing on the phone to me.' if params[:message].nil? || params[:message].empty?
+  
+  if @errors.empty?
+    Pony.mail(:to=>'george@ghickman.co.uk', :from=>"#{params[:email]}", :subject=>"#{params[:subject]}", :body=>"#{params[:message]}")
+    redirect '/index.html'
+  else
+    contact
+  end
+end
+
+
+
